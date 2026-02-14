bc7e88f3290c2294d6901b499ab458c43b2862ce
Aman-016 <amanmehta.am2004@gmail.com>
2026-02-14 17:11:15 +0530

    Add Sign Up option on frontend page

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
 
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+
 
 app.get("/", (req,res)=>{
     res.status(200).json({msg:"success from api"})
 })
 
-app.listen(ENV.PORT,()=>console.log("server is running on port " + ENV.PORT))
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
+
 

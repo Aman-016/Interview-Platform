75188bf764aa458775612bd2573fdca151495a72
Aman-016 <amanmehta.am2004@gmail.com>
2026-02-15 16:42:57 +0530

    Backend complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,46 +1,47 @@
 import express from "express";
-import cors from "cors";
 import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
+
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
-// Middleware
+const __dirname = path.resolve();
+
+// middleware
 app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-// Inngest endpoint
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
-// API routes
 app.get("/health", (req, res) => {
-  res.json({ msg: "api is up and running" });
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-app.get("/books", (req, res) => {
-  res.json({ msg: "this is the books endpoints" });
-});
-
-// ✅ Serve frontend in production (Render compatible)
+// make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
-  const frontendPath = path.join(process.cwd(), "backend", "dist");
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.use(express.static(frontendPath));
-
-  app.use((req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () =>
-      console.log(`Server is running on port: ${ENV.PORT}`)
-    );
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
     console.error("💥 Error starting the server", error);
   }
